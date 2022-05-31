d08cdbd694f5053162306190a4d571833271b57b
BeWoler <bewoler.del@gmail.com>
2022-05-31 12:22:20 +0400

    changed some styles & added description at home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/home.module.sass'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "./home.module.sass";
 
 const Home: NextPage = () => {
   return (
@@ -10,8 +10,15 @@ const Home: NextPage = () => {
         <meta name="description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={styles.description_box}>
+        <div className={styles.description}>
+          Hi! You have visited the Click.Net, our new social network, it is
+          currently under development, but you can also check out our finished
+          functionality here (◕‿◕)
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
